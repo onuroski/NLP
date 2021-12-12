a122207ab8c243723ed6721d1a152e4bdfbc1a4d
onuroski <56877309+onuroski@users.noreply.github.com>
2021-12-12 22:22:01 +0300

    Update and rename main.py to semantic analysis.py

--- semantic analysis.py
+++ semantic analysis.py
@@ -1,3 +1,5 @@
+#Basic semantic analysis of a Turkish corpus that will be downloaded from Internet will be done with statistical approaches. Accordingly, in the corpus; - a list of all words (vocabulary) and their frequencies, - a list of bigram and trigram tokens that have been used at least 5 times, - the most similar two words using Latent Semantic Analysis should be determined.
+
 from gensim import corpora
 from gensim.models import LsiModel
 from nltk.tokenize import RegexpTokenizer, word_tokenize,sent_tokenize
