2be6c348f6f534c2251fbbd01a8e23d9581fb043
onuroski <56877309+onuroski@users.noreply.github.com>
2021-12-12 22:13:45 +0300

    Update lesk algorithm.py

--- lesk algorithm.py
+++ lesk algorithm.py
@@ -1,3 +1,5 @@
+#A very simple example should be prepared to show how to do word sense disambiguation using the LESK algorithm on a Turkish corpus. Here, the student can be flexible about corpus and glossary, in other words, a corpus with a few sentences and a dictionary with a few definitions can be prepared.
+
 
 from snowballstemmer import TurkishStemmer
 
