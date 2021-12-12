f8dc0edc018b0ca2eb20c64c4f25ec757d20094b
onuroski <56877309+onuroski@users.noreply.github.com>
2021-12-12 22:16:51 +0300

    Update and rename task4.py to 2 POS Taggers.py

--- 2 POS Taggers.py
+++ 2 POS Taggers.py
@@ -1,3 +1,5 @@
+#A comparison will be made using different POS Taggers (at least two) on a corpus prepared for Turkish (the student will not prepare a new corpus, he/she must download it from internet, and the download link will be specified in the task). At least three words that POS Taggers labelled differently should be found and expressed.
+
 #zeyrek paketlerini indirdim
 import nltk
 from nltk import pos_tag
