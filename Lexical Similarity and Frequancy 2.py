635e814c51d385c073a6fa1727252f0849e969a3
onuroski <56877309+onuroski@users.noreply.github.com>
2022-01-11 02:24:59 +0300

    Update and rename task7_2.py to Lexical Similarity and Frequancy 2.py

--- Lexical Similarity and Frequancy 2.py
+++ Lexical Similarity and Frequancy 2.py
@@ -1,3 +1,7 @@
+#task 7 second version: First, low and high frequency words (for threshold = 5) should be determined in a Turkish collection. Words with low frequency will be assumed to have spelling errors. To solve this problem, a Lexical Similarity function will be used (student can find at internet or prepare) and it will be suggested that these erroneous assumed words can change to the most lexically similar word from high-frequency words. The code to be prepared must give a two-column list: low-frequency words in the first column and the most lexically similar high-frequency words in the second column.
+
+
+
 import difflib
 
 s1='nasılsınız'
