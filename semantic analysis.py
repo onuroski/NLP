7ddf86cba641d1a3b534751d13c65476a268b7f7
onuroski <56877309+onuroski@users.noreply.github.com>
2022-01-11 02:17:35 +0300

    Update semantic analysis.py

--- semantic analysis.py
+++ semantic analysis.py
@@ -1,4 +1,4 @@
-#Basic semantic analysis of a Turkish corpus that will be downloaded from Internet will be done with statistical approaches. Accordingly, in the corpus; - a list of all words (vocabulary) and their frequencies, - a list of bigram and trigram tokens that have been used at least 5 times, - the most similar two words using Latent Semantic Analysis should be determined.
+#Task 5: Basic semantic analysis of a Turkish corpus that will be downloaded from Internet will be done with statistical approaches. Accordingly, in the corpus; - a list of all words (vocabulary) and their frequencies, - a list of bigram and trigram tokens that have been used at least 5 times, - the most similar two words using Latent Semantic Analysis should be determined.
 
 from gensim import corpora
 from gensim.models import LsiModel
